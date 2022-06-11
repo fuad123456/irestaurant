a4508d6ba54ae41e80534b3eb3f4d397b36f30d0
Fuad Крым <fuadkrym@gmail.com>
2022-06-11 15:57:06 +0300

    feat: new slider

--- index.js
+++ index.js
@@ -1,41 +1,184 @@
-const slider=document.querySelector('.slider');
-const sliderImages=document.querySelectorAll('.slider img');
-const line=document.querySelector('.line');
-let pos
-let dif
-// function drag(){
-// 	// let posX=line
-// 	// console.dir(posX);
-// 	// console.log(clientX);
-// }
-// drag()
-line.addEventListener('mousedown',function(e){
-	e.preventDefault();
-	let posX=e.clientX
-	s(posX)
+let slider = document.querySelector('.slider'),
+  sliderList = slider.querySelector('.slider-list'),
+  sliderTrack = slider.querySelector('.slider-track'),
+  slides = slider.querySelectorAll('.slide'),
+  arrows = slider.querySelector('.slider-arrows'),
+  prev = arrows.children[0],
+  next = arrows.children[1],
+  slideWidth = slides[0].offsetWidth,
+  slideIndex = 0,
+  posInit = 0,
+  posX1 = 0,
+  posX2 = 0,
+  posY1 = 0,
+  posY2 = 0,
+  posFinal = 0,
+  isSwipe = false,
+  isScroll = false,
+  allowSwipe = true,
+  transition = true,
+  nextTrf = 0,
+  prevTrf = 0,
+  lastTrf = --slides.length * slideWidth,
+  posThreshold = slides[0].offsetWidth * 0.35,
+  trfRegExp = /([-0-9.]+(?=px))/,
+  getEvent = function() {
+    return (event.type.search('touch') !== -1) ? event.touches[0] : event;
+  },
+  slide = function() {
+    if (transition) {
+      sliderTrack.style.transition = 'transform .5s';
+    }
+    sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+
+    prev.classList.toggle('disabled', slideIndex === 0);
+    next.classList.toggle('disabled', slideIndex === --slides.length);
+  },
+  swipeStart = function() {
+    let evt = getEvent();
+
+    if (allowSwipe) {
+
+      transition = true;
+
+      nextTrf = (slideIndex + 1) * -slideWidth;
+      prevTrf = (slideIndex - 1) * -slideWidth;
+
+      posInit = posX1 = evt.clientX;
+      posY1 = evt.clientY;
+
+      sliderTrack.style.transition = '';
+
+      document.addEventListener('touchmove', swipeAction);
+      document.addEventListener('mousemove', swipeAction);
+      document.addEventListener('touchend', swipeEnd);
+      document.addEventListener('mouseup', swipeEnd);
+
+      sliderList.classList.remove('grab');
+      sliderList.classList.add('grabbing');
+    }
+  },
+  swipeAction = function() {
+
+    let evt = getEvent(),
+      style = sliderTrack.style.transform,
+      transform = +style.match(trfRegExp)[0];
+
+    posX2 = posX1 - evt.clientX;
+    posX1 = evt.clientX;
+
+    posY2 = posY1 - evt.clientY;
+    posY1 = evt.clientY;
+
+    // определение действия свайп или скролл
+    if (!isSwipe && !isScroll) {
+      let posY = Math.abs(posY2);
+      if (posY > 7 || posX2 === 0) {
+        isScroll = true;
+        allowSwipe = false;
+      } else if (posY < 7) {
+        isSwipe = true;
+      }
+    }
+
+    if (isSwipe) {
+      // запрет ухода влево на первом слайде
+      if (slideIndex === 0) {
+        if (posInit < posX1) {
+          setTransform(transform, 0);
+          return;
+        } else {
+          allowSwipe = true;
+        }
+      }
+
+      // запрет ухода вправо на последнем слайде
+      if (slideIndex === --slides.length) {
+        if (posInit > posX1) {
+          setTransform(transform, lastTrf);
+          return;
+        } else {
+          allowSwipe = true;
+        }
+      }
+
+      // запрет протаскивания дальше одного слайда
+      if (posInit > posX1 && transform < nextTrf || posInit < posX1 && transform > prevTrf) {
+        reachEdge();
+        return;
+      }
+
+      // двигаем слайд
+      sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+    }
+
+  },
+  swipeEnd = function() {
+    posFinal = posInit - posX1;
+
+    isScroll = false;
+    isSwipe = false;
+
+    document.removeEventListener('touchmove', swipeAction);
+    document.removeEventListener('mousemove', swipeAction);
+    document.removeEventListener('touchend', swipeEnd);
+    document.removeEventListener('mouseup', swipeEnd);
+
+    sliderList.classList.add('grab');
+    sliderList.classList.remove('grabbing');
+
+    if (allowSwipe) {
+      if (Math.abs(posFinal) > posThreshold) {
+        if (posInit < posX1) {
+          slideIndex--;
+        } else if (posInit > posX1) {
+          slideIndex++;
+        }
+      }
+
+      if (posInit !== posX1) {
+        allowSwipe = false;
+        slide();
+      } else {
+        allowSwipe = true;
+      }
+
+    } else {
+      allowSwipe = true;
+    }
+
+  },
+  setTransform = function(transform, comapreTransform) {
+    if (transform >= comapreTransform) {
+      if (transform > comapreTransform) {
+        sliderTrack.style.transform = `translate3d(${comapreTransform}px, 0px, 0px)`;
+      }
+    }
+    allowSwipe = false;
+  },
+  reachEdge = function() {
+    transition = false;
+    swipeEnd();
+    allowSwipe = true;
+  };
+
+sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
+sliderList.classList.add('grab');
+
+sliderTrack.addEventListener('transitionend', () => allowSwipe = true);
+slider.addEventListener('touchstart', swipeStart);
+slider.addEventListener('mousedown', swipeStart);
+
+arrows.addEventListener('click', function() {
+  let target = event.target;
+
+  if (target.classList.contains('next')) {
+    slideIndex++;
+  } else if (target.classList.contains('prev')) {
+    slideIndex--;
+  } else {
+    return;
+  }
+
+  slide();
 });
-line.addEventListener('mouseup',function drag(e){
-	e.preventDefault();
-	let posX=line
-	// console.dir(posX);
-	// console.log(e.clientX);
-	removeEventListener('mousemove',s);
-});
-function s(p){
-	line.addEventListener('mousemove',function d(e){
-		// console.log(e.clientX);
-		let px=p-e.clientX;
-		if(p>0){
-			line.style.left=-px+'px';
-		}else{
-			line.style.left=px+'px';
-		}
-	})
-	line.addEventListener('mouseup',function drag(e){
-		e.preventDefault();
-		let posX=line
-		// console.dir(posX);
-		// console.log(e.clientX);
-		removeEventListener('mousemove',s);
-	});
-}
